ce2e4edc6e6e85970cc3138b39343572bcacdfa4
ozgurozbekuk <ozgurozbek.uk@gmail.com>
2023-05-26 21:51:54 +0300

    react-random-color
    random color generator with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [hex,setHex] = useState("#ffffff");
+
+  function randomHex() {
+    const randomHex = "#" + Math.floor(Math.random() * 16777721).toString(16);
+    setHex(randomHex)
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundColor:`${hex}`}}>
+      <h1>{hex}</h1>
+      <button onClick={randomHex}>Click For Change Background Color</button>
+      <br/>
+      <button onClick={() => navigator.clipboard.writeText(hex)}>Copy the hex value</button>
     </div>
   );
 }
